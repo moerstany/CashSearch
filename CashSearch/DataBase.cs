684e927ad663a6403b12c8d1941f98fa45615fc8
moerstany <moerstany@gmail.com>
2021-09-11 15:44:47 +0700

    Update DataBase.cs

--- CashSearch/DataBase.cs
+++ CashSearch/DataBase.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace CashSearch
 {
+    
     class DataBase
     {
+        static void Main()
+        {
+            const string CONN_STR =???? ";Pwd=269f43dc;";
+            var data_base = new MySqlConnection(CONN_STR);
+            data_base.Open();
+​
+            ShowInfo("");
+            ShowInfo("");
+            ShowInfo("");
+            var select = Console.ReadLine();
+                        
+            var sql = $"SELECT ???? FROM tab_ JOIN tab_ ON tab??? = tab_products.id WHERE product_id = {select}";
+            var command = new MySqlCommand
+            {
+                CommandText = sql,
+                Connection = data_base
+            };
+            var ??? = command.ExecuteReader();
+​
+            if (???.HasRows)
+            {
+                do
+                {
+                    ???.Read();
+                    var count = ???.GetInt32("count");
+                    ShowSuccess($"count = {count}");
+                } while (???.NextResult());
+            }
+            else
+            {
+                ShowError("Вернулась пустая таблица");
+            }
+            
+            data_base.Close();
+        }
     }
 }
+   
